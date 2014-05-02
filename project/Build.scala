0ad5d31254ed48f92e426ec425d4029b220ef906
Christopher Vogt <christopher.vogt@epfl.ch>
2014-05-02 13:59:23 +0200

    Release 2.0.2-RC1

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object myBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       scalaVersion := "2.10.3",
       libraryDependencies ++= List(
-        "com.typesafe.slick" %% "slick" % "2.0.1",
+        "com.typesafe.slick" %% "slick" % "2.0.2-RC1",
         "org.slf4j" % "slf4j-nop" % "1.6.4",
         "com.h2database" % "h2" % "1.3.170"
       ),
